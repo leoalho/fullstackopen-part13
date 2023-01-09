b44ffbc2a4e67c7551830ecdb4ae584f67ec8620
Leo <leo.a.alho@gmail.com>
2023-01-09 14:12:41 +0200

    Exercise 13.6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,10 @@
 const blogRouter = require('express').Router()
 const { Blog } = require('../models')
 
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()}
+
 blogRouter.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
@@ -17,18 +21,29 @@ blogRouter.post('/', async (req, res) => {
     }
 })
 
-blogRouter.delete('/:id', async (req, res) => {
-    try{
-        await Blog.destroy({
-            where: {
-                id: parseInt(req.params.id)
-            }
-        })
-        res.status(200).end()
-    } catch (error) {
-        console.log(error)
+blogRouter.get('/:id', blogFinder, (req, res) => {
+    if (req.blog) {
+        res.json(req.blog)
+    }else{
         res.status(400).end()
     }
+})
+
+blogRouter.put('/:id', blogFinder, async (req, res) => {
+    if (req.blog && req.body.likes) {
+        req.blog.likes = req.body.likes
+        await req.blog.save()
+        res.json(req.blog)
+    } else {
+        res.status(404).end()
+    }
+})
+
+blogRouter.delete('/:id', blogFinder, async (req, res) => {
+    if (req.blog) {
+        await req.blog.destroy()
+    }
+    res.status(204).end()
 })
 
 module.exports = blogRouter
