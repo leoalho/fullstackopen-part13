33d884594956338c8a670af0353e1f291edf7d83
Leo <leo.a.alho@gmail.com>
2023-01-10 12:00:07 +0200

    Exercises 13.14 && 13.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -26,7 +26,20 @@ const blogFinder = async (req, res, next) => {
 blogRouter.get('/', async (req, res) => {
     var where = {}
     if (req.query.search){
-        where.title = {[Op.iLike]: `%${req.query.search}%`}
+        where = {
+            [Op.or]: [
+                {
+                    title: {
+                        [Op.iLike]: `%${req.query.search}%`
+                    }
+                },
+                {
+                    author: {
+                        [Op.iLike]: `%${req.query.search}%`
+                    }
+                }
+            ]
+        }
     }
     
     const blogs = await Blog.findAll({
