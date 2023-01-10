224b048210424f4a5dfe4dae820f383c7ae1d3d7
Leo <leo.a.alho@gmail.com>
2023-01-10 10:20:10 +0200

    Exercise 13.9

--- util/middleware.js
+++ util/middleware.js
@@ -1,8 +1,6 @@
 const errorHandler = (error, request, response, next) => {
     console.log(error.name)
-    if (error.name === 'CastError') {
-      return response.status(400).json({ error: 'malformatted id' })
-    } else if (error.name === 'SequelizeValidationErrorr' || error.name === 'MongoServerError') {
+    if (error.name === 'SequelizeValidationErrorr') {
       return response.status(400).json({ error: error.message })
     }
     next(error)
